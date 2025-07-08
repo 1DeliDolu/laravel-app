901d14d11504ad10fc28f544b119a7b23cdc537a
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2025-07-08 21:49:11 +0200

    lesson 7

--- laravel-app/resources/views/components/layout.blade.php
+++ laravel-app/resources/views/components/layout.blade.php
@@ -4,13 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ninja Network</title>
+
+  @vite('resources/css/app.css')
 </head>
 <body>
 
   <header>
     <nav>
       <h1>Ninja Network</h1>
-      <a href="/ninjas">
+      <a href="/ninjas"> 
         All Ninjas
       </a>
       <a href="/ninjas/create">
